f75ce8562bb419e012ecc81ecc0d81e1d468ee77
Gion-Andri Cantieni <gion-andri@gmx.ch>
2026-04-29 17:24:37 +0200

    chore: migrate from deprecated TSLint to ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ['src/**/*.ts'],
+  }
+);
